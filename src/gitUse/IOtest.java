17bd83a5195704fd758743da91c72793454fc66b
Ilya <i.laukhin@hybrid.ru>
2020-07-15 16:02:44 +0300

    Добавления функции сложения

---
+++ src/gitUse/IOtest.java
@@ -0,0 +1,20 @@
+package gitUse;
+
+public class IOtest {
+
+    public static void main(String[] args) {
+
+        for(int id = 1; id <= 200; id++){
+            double coff = (Math.random() +  Math.random()) * id * 100;
+            System.out.println((int)coff);
+        }
+
+        int result = additionTwoIntNumbers(4, 3);
+        System.out.println("\n\n" + result);
+    }
+
+    // овый метод для складывания чисел
+    private static int additionTwoIntNumbers(int number_1, int number_2){
+        return number_1 + number_2;
+    }
+}
