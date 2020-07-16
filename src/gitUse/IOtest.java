eb5d63f05e275bb13a4562a08fedfa50fd821b79
Ilya <i.laukhin@hybrid.ru>
2020-07-16 11:28:37 +0300

    еще коммит

--- src/gitUse/IOtest.java
+++ src/gitUse/IOtest.java
@@ -4,17 +4,22 @@ public class IOtest {
 
     public static void main(String[] args) {
 
-        for(int id = 1; id <= 200; id++){
-            double coff = (Math.random() +  Math.random()) * id * 100;
-            System.out.println((int)coff);
-        }
+        ganarRandomNumbers();
 
         int result = additionTwoIntNumbers(4, 3);
         System.out.println("\n\n" + result);
+
     }
 
     // новый метод для складывания чисел
     private static int additionTwoIntNumbers(int number_1, int number_2){
         return number_1 + number_2;
+    }
+
+    private static void ganarRandomNumbers(){
+        for(int id = 1; id <= 200; id++){
+            double coff = (Math.random() +  Math.random()) * id * 100;
+            System.out.println((int)coff);
+        }
     }
 }
